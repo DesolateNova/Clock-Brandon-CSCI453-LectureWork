e46041cd499545cf5cfcf0c8105c2b50ca448549
DesolateNova <BrandonClock1@Gmail.com>
2025-03-05 14:39:13 -0500

    ProCom
    Refactoring and commenting on Billion Collision behavior

--- Clock-Brandon-BillionsProjet/Assets/Scripts/BillionsBehavior.cs
+++ Clock-Brandon-BillionsProjet/Assets/Scripts/BillionsBehavior.cs
@@ -6,6 +6,7 @@ public class BillionsBehavior : MonoBehaviour
 
     
     private float billionRadius;
+    private Vector3 myPos;
     //private Collider2D[] result;
     private string color;
 
@@ -25,6 +26,7 @@ public class BillionsBehavior : MonoBehaviour
         {
             transform.position += new Vector3(Random.Range(-billionRadius, billionRadius), Random.Range(-billionRadius, billionRadius), 0f);
         }*/
+        myPos = gameObject.transform.position;
         if (ProxyManager.worldSpawnlings[color] != null && ProxyManager.worldSpawnlings[color].Count > 1)
             positionAdjuster();
 
@@ -39,64 +41,57 @@ public class BillionsBehavior : MonoBehaviour
         foreach (string color in ProxyManager.worldSpawnlings.Keys)
         {
             foreach (GameObject billion in ProxyManager.worldSpawnlings[color])
-            {
-                if (billion.GetInstanceID().Equals(this.GetInstanceID()))
+            {   
+                //Get other billions location if it is greater than the diameter of this billion then it is not close enough to consider
+                Vector3 otherPos = billion.transform.position;
+                float distance = Vector3.Distance((this.transform.position), billion.transform.position);
+                if (billion.GetInstanceID().Equals(this.GetInstanceID()) || distance > billionRadius * 2f)
                     continue;
 
+                //Get the radius of the other billion for use in operations
                 CircleCollider2D other = billion.GetComponent<CircleCollider2D>();
-
+    
+                //Fail-safe
                 if (other == null)
                     return;
 
-                float distance = Vector3.Distance((this.transform.position), billion.transform.position);
+                //Get relevant information needed for any operation:
+                //size of the other billions radius
                 float otherRadius = other.radius;
-                float xOffset = Random.Range(-1f, 1f);
-                float yOffset = Random.Range(-1f, 1f);
-                
 
-                Vector3 direction = (billion.transform.position - this.transform.position).normalized;
-                Vector3 otherDireciton = -direction;
-                Vector3 radiusPoint = transform.position + (direction * billionRadius);
-                Vector3 otherPointOnRadius = billion.transform.position + (-direction * otherRadius);
+
+                //Direction to and from other
+                Vector3 directionTo = (billion.transform.position - this.transform.position).normalized;
+                Vector3 directionToFrom = -directionTo;
+
+                Vector3 radiusPoint = transform.position + (directionTo * billionRadius);
+                Vector3 otherPointOnRadius = billion.transform.position + (-directionTo * otherRadius);
+
+                //Get the ratio of overlap. ditance that other point is from center point of this billion divided by the radius
                 float ourOverlap = Vector3.Distance(radiusPoint, otherPointOnRadius);
 
+                
+                //Generate x and y offset for dynamic push directionTo
+                float xOffset = Random.Range(-1f, 1f);
+                float yOffset = Random.Range(-1f, 1f);
+
+
+                //If the billions are on top of eachother, push them awayfrom eachother at an equal rate toward a the arena center
                 if (distance == 0)
                 {
                     Vector3 arenaCenter = GameObject.Find("ArenaCenter").transform.position;
                     if (arenaCenter != null)
                     {
-                        direction = (arenaCenter - this.transform.position).normalized;
-                        billion.transform.position += direction * billionRadius;
-                        this.transform.position -= direction * billionRadius;
+                        directionTo = (arenaCenter - this.transform.position).normalized;
+                        billion.transform.position += directionTo * billionRadius;
+                        this.transform.position -= directionTo * billionRadius;
                     }
                     return;
                 }
 
-                if (distance <= billionRadius)
-                {
-                    direction = new Vector3(xOffset, yOffset, 0f);
-                    PushOther(billion, direction, 0.01f, distance, 0);
-                    return;
-                }
-                else if (Vector3.Distance(otherPointOnRadius, transform.position) <= billionRadius)
-                {
-                    direction = new Vector3(xOffset, yOffset, 0f);
-                    PushOther(billion, direction, 0.01f, distance, 0);
-                    return;
-                }
+
+
             }
         }
-    }
-
-    private void PushOther(GameObject other, Vector3 direction, float force, float distance, int numberOfTimes)
-    {
-        if (distance <= 0f)
-        {
-            other.transform.position += (direction * force) * numberOfTimes;
-            Debug.Log("Number Of Times: " + numberOfTimes);
-            return;
-        }
-        numberOfTimes++;
-        PushOther(other, direction, force, distance - force, numberOfTimes);
     }
 }
