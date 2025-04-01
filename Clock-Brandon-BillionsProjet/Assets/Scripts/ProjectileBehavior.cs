9b2f72d2a62661272f3dc9e2479efecb07c581b3
Bclock <bclock@iu.edu>
2025-04-01 15:42:54 -0400

    ProgCom

--- Clock-Brandon-BillionsProjet/Assets/Scripts/ProjectileBehavior.cs
+++ Clock-Brandon-BillionsProjet/Assets/Scripts/ProjectileBehavior.cs
@@ -11,13 +11,19 @@ public class ProjectileBehavior : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        y = Mathf.Abs(Mathf.Tan((transform.rotation.eulerAngles.z -90f))) * Mathf.Rad2Deg % 360f;
-        if (y == 90 || y == 270)
-            x = 0;
-        else if (y > 90 && y < 270)
+        float angle = transform.rotation.eulerAngles.z + 90f;
+        y = Mathf.Abs(Mathf.Tan(angle * Mathf.Deg2Rad)) % 360f;
+
+        if (angle > 180 && angle < 360)
+            y =  -y;
+    
+        if (angle > 90 && angle < 270)
             x = -1;
+        else if (angle == 90 || angle == 270)
+            x = 0;
         else
             x = 1;
+
 
         Debug.Log($"x value: {x}");
         Debug.Log($"y value: {y}");
